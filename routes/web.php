afddfaa71183bd71435f5dca5f4a901f476c5cac
B1910132_Cao Thị Huỳnh Quyên <quynhquyenhhn@gmail.com>
2023-07-20 17:13:53 +0700

    auth

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth', 'is_admin'])->group(function (){
+    Route::get('/dashboard', function (){
+        return "this is admin";
+    });
+});
+
